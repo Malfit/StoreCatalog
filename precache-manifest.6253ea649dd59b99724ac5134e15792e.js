0785df0bc94b95367eacb593eb5b664d22a17864
Malfit <oleksandr.rybakov@nure.ua>
2020-05-21 22:46:56 +0300

    Updates

---
+++ precache-manifest.6253ea649dd59b99724ac5134e15792e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56f44c4b7f26b45b66325f666ce778bd",
+    "url": "/StoreCatalog/index.html"
+  },
+  {
+    "revision": "0f4c75bfbdf8e7508a2b",
+    "url": "/StoreCatalog/static/css/main.ac5ad94a.chunk.css"
+  },
+  {
+    "revision": "88ecd24ec7edfbc6145c",
+    "url": "/StoreCatalog/static/js/2.f0442236.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/StoreCatalog/static/js/2.f0442236.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f4c75bfbdf8e7508a2b",
+    "url": "/StoreCatalog/static/js/main.0842fe98.chunk.js"
+  },
+  {
+    "revision": "962b06d65aeda12a625a",
+    "url": "/StoreCatalog/static/js/runtime-main.2b65ab5e.js"
+  }
+]);
